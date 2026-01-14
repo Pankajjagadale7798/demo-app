473652360f23189e8e6cbba1bdecf8eff2311702
Pankajjagadale7798 <pankajjagdale123.sitit@gmail.com>
2026-01-14 20:40:59 +0530

    find second large ele

---
+++ src/practice_pgm/FindSecondLargest.java
@@ -0,0 +1,26 @@
+package practice_pgm;
+
+public class FindSecondLargest {
+    public static void main(String[] args) {
+
+
+        int arr[] = {20, 52, 62, 42, 70, 41, 23};
+
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+
+                second = largest;
+                largest = arr[i];
+
+        } else if (arr[i]>second) {
+                   second=arr[i];
+            }
+            }
+            System.out.println(second);
+        System.out.println(largest);
+
+        }
+    }
